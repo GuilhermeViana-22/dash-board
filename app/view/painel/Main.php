cb4365cc38333e60c7a19fbd5a2c0d878d1e2627
Guilherme Viana <gguicido.viana@gmail.com>
2021-10-30 22:19:23 -0300

    modificaçãos no escopo das tabelas, foi alterado o modo de visualização das tabelas

--- app/view/painel/Main.php
+++ app/view/painel/Main.php
@@ -1,3 +1,174 @@
+<?php
+
+
+
+    use App\Model\ClassSelect;
+
+    $obj = new ClassSelect;
+
+   $tabela = $obj->selectDadosBancoCores('azul');
+
+    var_dump($tabela);
+    
+?>
+
+
+
 <main>
-    <div class="container_content_cor"></div>
+
+    <div class="container_content_cor">
+
+    <div class="box_cor">
+    <div class="box_cor_top" style="background-color: #10be">
+                <div class="nome_cor">
+                    <span>
+                        Linha de promocao <br> azul
+                    </span>
+                </div>
+    
+                <div class="box_ver_link">
+                    <a href="http://localhost/dash-board/read-where/selecionarCor/azul">Ver Link</a>
+                </div>
+            </div>
+            <div class="box_bottom_info_cor">
+                <div class="box_info_bottom_cor">
+                    <span>Qnt Pessoas utilizando:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+    
+                <div class="box_info_bottom_cor">
+                    <span>Qnt de Items:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+    
+                <div class="box_info_bottom_cor">
+                    <span>Posicao na fila:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+            </div>
+        
+    </div>
+    <!-- div amarelas -->
+    <div class="box_cor">
+    <div class="box_cor_top" style="background-color: yellow">
+                <div class="nome_cor">
+                    <span>
+                        Linha de promocao <br>amarelo
+                    </span>
+                </div>
+    
+                <div class="box_ver_link">
+                    <a href="http://localhost/dash-board/read-where/selecionarCor/amarelo">Ver Link</a>
+                </div>
+            </div>
+            <div class="box_bottom_info_cor">
+                <div class="box_info_bottom_cor">
+                    <span>Qnt Pessoas utilizando:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+    
+                <div class="box_info_bottom_cor">
+                    <span>Qnt de Items:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+    
+                <div class="box_info_bottom_cor">
+                    <span>Posicao na fila:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+            </div>
+       
+    </div>
+    <!-- cor vermelha la lista -->
+    <div class="box_cor">
+    <div class="box_cor_top" style="background-color: red">
+                <div class="nome_cor">
+                    <span>
+                        Linha de promocao <br> vermelho
+                    </span>
+                </div>
+    
+                <div class="box_ver_link">
+                    <a href="http://localhost/dash-board/read-where/selecionarCor/vermelho">Ver Link</a>
+                </div>
+            </div>
+            <div class="box_bottom_info_cor">
+                <div class="box_info_bottom_cor">
+                    <span>Qnt Pessoas utilizando:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+    
+                <div class="box_info_bottom_cor">
+                    <span>Qnt de Items:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+    
+                <div class="box_info_bottom_cor">
+                    <span>Posicao na fila:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+            </div>
+        
+    </div>
+    <!-- cor verde -->
+    <div class="box_cor">
+    <div class="box_cor_top" style="background-color: green">
+                <div class="nome_cor">
+                    <span>
+                        Linha de promocao <br> Verde
+                    </span>
+                </div>
+    
+                <div class="box_ver_link">
+                    <a href="http://localhost/dash-board/read-where/selecionarCor/verde">Ver Link</a>
+                </div>
+            </div>
+            <div class="box_bottom_info_cor">
+                <div class="box_info_bottom_cor">
+                    <span>Qnt Pessoas utilizando:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+    
+                <div class="box_info_bottom_cor">
+                    <span>Qnt de Items:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+    
+                <div class="box_info_bottom_cor">
+                    <span>Posicao na fila:</span>
+                    <div class="box_valor_info_bottom">
+                        0
+                    </div>
+                </div>
+            </div>
+        
+    </div>
+
+    </div>
+ 
+
+
 </main>
+
