78186ea5f52efa86374a88cc3be640933ac1720f
Guilherme Viana <gguicido.viana@gmail.com>
2021-10-31 02:51:29 -0300

    xxx

--- app/controller/ControllerPhpMailer.php
+++ app/controller/ControllerPhpMailer.php
@@ -17,26 +17,34 @@ $mail = new PHPMailer(true);
 try {
 
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'riancontatoprofissional@gmail.com';
-    $mail->Password   = "<senha aqui>";
-    $mail->SMTPSecure = 'TSL';
-    $mail->Port       = 587;
+    $mail->IsSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "tls";
+    $mail->Username = "gguicido.viana@gmail.com";
+    $mail->Password = "1Marmaduki";
+    $mail->Port = 587;
 
     //Recipients
-    $mail->setFrom('riancontatoprofissional@gmail.com', 'Mailer');
-    $mail->addAddress('riancontatoprofissional@gmail.com', 'Joe User');     //Add a recipient
+    $mail->setFrom('gguicido.viana@gmail.com', 'Guilherme Augusto');
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->addAddress('gguicido.viana@gmail.com', 'Guilherme Augusto');    //Add a recipient 
+    
+
+
+    $mail->isHTML(true);   //Set email format to HTML 
+                                  
     $mail->Subject = 'Here is the subject';
+
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
+
     echo 'Message has been sent';
+
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+
+    echo "Não Foi Possivel enviar a mensagem . Mailer Error: {$mail->ErrorInfo}";
 }
